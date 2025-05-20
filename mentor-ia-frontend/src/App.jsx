a7cadd211e06a55cf3d44cf9cf3eeec4c3009de3
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-05-20 16:16:07 -0500

    templates for material uploading

--- mentor-ia-frontend/src/App.jsx
+++ mentor-ia-frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Course_ModuleList from './pages/Course_ModuleList';
+import CreateMaterialForm from './pages/Course_MaterialForm';
 import './App.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/courses/:idCourse/modules" element={<Course_ModuleList />} />
+        <Route path="/courses/:idCourse/modules/:idModule/materials/new" element={<CreateMaterialForm/>} />
         {/*  */}
       </Routes>
     </Router>
