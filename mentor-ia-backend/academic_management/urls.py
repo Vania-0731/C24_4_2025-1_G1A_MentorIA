53ac1d91cf9447992c845af8c650e1d382210376
abuenoh-tecsup <alvaro.bueno@tecsup.edu.pe>
2025-05-20 16:08:42 -0500

    changed names for tables Class and Course to Course and Subject respectively

--- mentor-ia-backend/academic_management/urls.py
+++ mentor-ia-backend/academic_management/urls.py
@@ -2,14 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     CareerViewSet, CourseViewSet, AcademicPeriodViewSet,
-    ClassViewSet, EnrollmentViewSet
+    SubjectViewSet, EnrollmentViewSet
 )
 
 router = DefaultRouter()
 router.register(r'careers', CareerViewSet)
-router.register(r'courses', CourseViewSet)
+router.register(r'subjects', SubjectViewSet)
 router.register(r'academic-periods', AcademicPeriodViewSet)
-router.register(r'classes', ClassViewSet)
+router.register(r'courses', CourseViewSet)
 router.register(r'enrollments', EnrollmentViewSet)
 
 urlpatterns = [
