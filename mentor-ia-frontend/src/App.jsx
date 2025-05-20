13219b3174c0b30cf0511e07d7f1f3f7ee47272d
Eduardo <eduardobullonvera@gmail.com>
2025-05-20 16:22:40 -0500

    Update App Component

--- mentor-ia-frontend/src/App.jsx
+++ mentor-ia-frontend/src/App.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Courses from './pages/Courses';
+import Materials from './pages/Materials';
+import Tasks from './pages/Tasks';
+import Grades from './pages/Grades';
+import Forums from './pages/Forums';
+import Profile from './pages/Profile';
+import Rules from './components/home/Rules';
+
+import Navbar from './components/common/Navbar';
+import Sidebar from './components/common/Sidebar';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/*  */}
-      </Routes>
+      <Navbar />
+      <Sidebar />
+      <div className="app-content"> {}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/materials" element={<Materials />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/grades" element={<Grades />} />
+          <Route path="/forums" element={<Forums />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/rules" element={<Rules />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
