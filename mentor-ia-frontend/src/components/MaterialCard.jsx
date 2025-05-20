ff7d1a411719f847a5247b825b6c8f91580fd5d1
Eduardo <eduardobullonvera@gmail.com>
2025-05-20 03:35:39 -0500

    Delete Components

--- mentor-ia-frontend/src/components/MaterialCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const MaterialCard = ({ title, type }) => (
-  <div className="card">
-    <strong>{title}</strong> <em>({type})</em>
-  </div>
-);
-
-export default MaterialCard;
