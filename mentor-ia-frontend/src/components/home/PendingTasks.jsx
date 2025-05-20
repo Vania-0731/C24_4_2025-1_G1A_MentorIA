ee7f32b6e1829b393534ca1d983d20c0f821f73f
Eduardo <eduardobullonvera@gmail.com>
2025-05-20 16:23:43 -0500

    Update PendingTask Component

--- mentor-ia-frontend/src/components/home/PendingTasks.jsx
+++ mentor-ia-frontend/src/components/home/PendingTasks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './PendingTasks.css';
 
 const PendingTasks = () => {
@@ -49,7 +50,7 @@ const PendingTasks = () => {
           </div>
         ))}
       </div>
-      <button className="view-all-btn">Ver todas las tareas</button>
+      <Link to="/tasks" className="view-all-btn">Ver todas las tareas</Link>
     </div>
   );
 };
